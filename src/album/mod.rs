426da12ec05ddd0256e50c444886a731c21bdd24
Clément Nerma <clement.nerma@gmail.com>
2023-07-12 11:08:48 +0200

    Download thumbnail alongside album tracks

--- src/album/mod.rs
+++ src/album/mod.rs
@@ -1,8 +1,9 @@
 mod cmd;
 
 use std::{
+    collections::HashSet,
     fs,
-    path::Path,
+    path::{Path, PathBuf},
     time::{SystemTime, UNIX_EPOCH},
 };
 
@@ -14,9 +15,9 @@ use serde::Deserialize;
 use crate::{
     config::Config,
     dl::{download, DlArgs},
-    info, info_inline,
-    platforms::build_platform_matchers,
-    success,
+    info,
+    platforms::{build_platform_matchers, PlatformsMatchers},
+    success, warn,
     ytdlp::{fetch_playlist, RawPlaylist},
 };
 
@@ -28,12 +29,12 @@ pub fn download_album(args: AlbumArgs, config: &Config, cwd: &Path) -> Result<()
 
     let platform_matchers = build_platform_matchers(config)?;
 
-    info!("|\n| Part 1/4: Fetching playlist...\n|\n");
+    info!("|\n| Part 1/5: Fetching playlist...\n|\n");
 
     let RawPlaylist { entries } = fetch_playlist(&config.yt_dlp_bin, &url)?;
 
     info!(
-        "|\n| Part 2/4: Downloading {} tracks...\n|\n",
+        "|\n| Part 2/5: Downloading {} tracks...\n|\n",
         entries.len().to_string().bright_yellow()
     );
 
@@ -86,7 +87,7 @@ pub fn download_album(args: AlbumArgs, config: &Config, cwd: &Path) -> Result<()
         info!("");
     }
 
-    info!("|\n| Part 3/4: Analyzing tracks metadata...\n|\n");
+    info!("|\n| Part 3/5: Analyzing tracks metadata...\n|\n");
 
     let dl_files = fs::read_dir(&tmp_dir)
         .context("Failed to read temporary download directory")?
@@ -95,9 +96,10 @@ pub fn download_album(args: AlbumArgs, config: &Config, cwd: &Path) -> Result<()
 
     let mut initial_track_metadata = None;
     let mut moves = vec![];
+    let mut treated_json_files = HashSet::new();
 
     for (i, dl_file) in dl_files
-        .into_iter()
+        .iter()
         .filter(|c| c.path().extension().unwrap() != "json")
         .enumerate()
     {
@@ -107,7 +109,7 @@ pub fn download_album(args: AlbumArgs, config: &Config, cwd: &Path) -> Result<()
             continue;
         }
 
-        info_inline!(
+        info!(
             "| Analyzing track {:>counter_len$} / {}...",
             (i + 1).to_string().bright_yellow(),
             entries.len().to_string().bright_yellow(),
@@ -126,15 +128,27 @@ pub fn download_album(args: AlbumArgs, config: &Config, cwd: &Path) -> Result<()
                 )
             })?;
 
+        treated_json_files.insert(json_path);
+
         let TrackMetadata {
             album,
             uploader,
             track,
         } = &track_metadata;
 
-        match initial_track_metadata {
-            None => initial_track_metadata = Some(track_metadata.clone()),
-            Some(ref initial_mt) => {
+        let album_dir = match initial_track_metadata {
+            None => {
+                let album_dir = cwd.join(format!("{uploader} - {album}"));
+
+                if !album_dir.exists() {
+                    fs::create_dir(&album_dir).context("Failed to create album directory")?;
+                }
+
+                initial_track_metadata = Some((track_metadata.clone(), album_dir.clone()));
+
+                album_dir
+            }
+            Some((ref initial_mt, ref album_dir)) => {
                 if album != &initial_mt.album {
                     bail!(
                         "Album mismatch: expected '{}', found '{}'",
@@ -150,14 +164,10 @@ pub fn download_album(args: AlbumArgs, config: &Config, cwd: &Path) -> Result<()
                         uploader.bright_yellow()
                     );
                 }
+
+                album_dir.clone()
             }
-        }
-
-        let album_dir = cwd.join(format!("{uploader} - {album}"));
-
-        if !album_dir.exists() {
-            fs::create_dir(&album_dir).context("Failed to create album directory")?;
-        }
+        };
 
         let file_ext = dl_file.extension().unwrap().to_str().unwrap();
 
@@ -166,11 +176,82 @@ pub fn download_album(args: AlbumArgs, config: &Config, cwd: &Path) -> Result<()
         moves.push((dl_file, track_file));
     }
 
-    info!("|\n| Part 4/4: Copying files to destination...\n|\n");
+    if initial_track_metadata.is_none() {
+        bail!("No track found in playlist");
+    }
+
+    info!("");
+    info!("|\n| Part 4/5: Downloading album thumbnail...\n|\n");
+
+    info!("| Downloading playlist metadata...");
+
+    let album_json_file = download_single_file(
+        &tmp_dir,
+        &url,
+        cookie_profile.clone(),
+        config,
+        &platform_matchers,
+        vec![
+            "--flat-playlist".to_string(),
+            "--write-info-json".to_string(),
+            "--skip-download".to_string(),
+        ],
+    )?;
+
+    let album_infos =
+        fs::read_to_string(&album_json_file).context("Failed to read album informations file")?;
+
+    let AlbumMetadata { thumbnails } = serde_json::from_str::<AlbumMetadata>(&album_infos)
+        .context("Failed to parse album metadata")?;
+
+    let thumbnail = thumbnails
+        .iter()
+        .filter(|thumb| {
+            // HACK: Fix for Youtube returning non-existing URLs
+            !(thumb.url.contains(".ytimg.com") && thumb.width > 1000 && thumb.height > 1000)
+        })
+        .max_by_key(|thumb| {
+            // Converting to higher-capacity number to avoid overflows
+            u64::from(thumb.width) * u64::from(thumb.height)
+        });
+
+    match thumbnail {
+        None => warn!("Warning: album has no thumbnail!"),
+        Some(AlbumThumbnail {
+            url,
+            height: _,
+            width: _,
+        }) => {
+            info!("| Downloading thumbnail at: {}", url.bright_magenta());
+
+            let thumbnail_file = download_single_file(
+                &tmp_dir,
+                url,
+                cookie_profile,
+                config,
+                &platform_matchers,
+                vec![],
+            )?;
+
+            moves.insert(
+                0,
+                (
+                    thumbnail_file.clone(),
+                    initial_track_metadata.unwrap().1.join(format!(
+                        "cover.{}",
+                        thumbnail_file.extension().unwrap().to_str().unwrap()
+                    )),
+                ),
+            );
+        }
+    }
+
+    info!("");
+    info!("|\n| Part 5/5: Copying files to destination...\n|\n");
 
     for (dl_file, track_file) in moves {
         info!(
-            " |> {}",
+            "| Copying to: {}",
             track_file
                 .strip_prefix(cwd)
                 .unwrap()
@@ -179,12 +260,52 @@ pub fn download_album(args: AlbumArgs, config: &Config, cwd: &Path) -> Result<()
         );
 
         fs::copy(&dl_file, track_file).context("Failed to copy track file to destination")?;
-        fs::remove_file(&dl_file).context("Failed to remove temporary downloaded file")?;
     }
+
+    fs::remove_dir_all(&tmp_dir).context("Failed to remove the temporary download directory")?;
 
     success!("Done!");
 
     Ok(())
+}
+
+fn download_single_file(
+    tmp_dir: &Path,
+    url: &str,
+    cookie_profile: Option<String>,
+    config: &Config,
+    platform_matchers: &PlatformsMatchers,
+    forward: Vec<String>,
+) -> Result<PathBuf> {
+    let now = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+    let tmp_dir = tmp_dir.join(format!("{}-{}-single", now.as_secs(), now.subsec_micros()));
+
+    fs::create_dir(&tmp_dir).context("Failed to create a temporary download directory")?;
+
+    download(
+        DlArgs {
+            url: url.to_string(),
+            no_temp_dir: true,
+            output_dir: Some(tmp_dir.clone()),
+            skip_repair_date: true,
+            no_platform: true,
+            cookie_profile,
+            forward,
+            ..Default::default()
+        },
+        config,
+        platform_matchers,
+        None,
+    )?;
+
+    let dl_files = fs::read_dir(&tmp_dir)
+        .context("Failed to read temporary download directory")?
+        .collect::<Result<Vec<_>, _>>()
+        .context("Failed to iterate over content of the temporary download directory")?;
+
+    assert_eq!(dl_files.len(), 1, "Expected exactly one file");
+
+    Ok(dl_files[0].path())
 }
 
 fn extract_json_track_metadata(json_path: &Path) -> Result<TrackMetadata> {
@@ -207,4 +328,16 @@ struct TrackMetadata {
     uploader: String,
     track: String,
     // release_year: u16,
+}
+
+#[derive(Deserialize)]
+struct AlbumMetadata {
+    thumbnails: Vec<AlbumThumbnail>,
+}
+
+#[derive(Deserialize)]
+struct AlbumThumbnail {
+    url: String,
+    height: u16,
+    width: u16,
 }
