d6dca90366afa011f8ca5c200542c15609093e9d
Clément Nerma <clement.nerma@gmail.com>
2024-11-04 19:04:44 +0100

    Improve file sanitization method

--- src/utils/filenames.rs
+++ src/utils/filenames.rs
@@ -1,34 +1,12 @@
 pub fn sanitize_filename(filename: &str) -> String {
     filename
-        .replace('"', "'")
-        .replace(": ", " - ")
-        .chars()
-        .map(|c| {
-            if c.is_alphanumeric()
-                || c == ' '
-                || c == '-'
-                || c == '_'
-                || c == '.'
-                || c == '('
-                || c == ')'
-                || c == '['
-                || c == ']'
-                || c == '{'
-                || c == '}'
-                || c == '!'
-                || c == '\''
-                || c == '’'
-                || c == '°'
-                || c == '#'
-                || c == '&'
-                || c == '$'
-                || c == '^'
-                || c == '@'
-            {
-                c
-            } else {
-                '_'
-            }
-        })
-        .collect()
+        .replace('/', "\u{1735}")
+        .replace('\\', "\u{29F5}")
+        .replace('|', "\u{2223}")
+        .replace('<', "\u{02C2}")
+        .replace('>', "\u{02C3}")
+        .replace(':', "\u{0589}")
+        .replace('"', "\u{02BA}")
+        .replace('?', "\u{FF1F}")
+        .replace('*', "\u{2217}")
 }
